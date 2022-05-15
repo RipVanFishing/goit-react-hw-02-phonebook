a7f5134c71f41977c2a2821dad2e345b6b93786b
Denis Serkhel <ripvanfish27@gmail.com>
2022-05-15 12:33:35 +0200

    добавил стили

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,5 +1,7 @@
 
 import React from "react";
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import {AiFillPhone} from 'react-icons/ai'
 import css from './Contacts.module.css';
 
 
@@ -10,8 +12,9 @@ const Contacts = ({contacts,onDelete}) => {
             <ul className={css.list}>
                 
                 {contacts.map((el) => (<li key={el.id} className={css.item_name}>
+                    <AiFillPhone/>
                 <p className={css.text_name}>{el.name}: {el.number}</p>
-                <button type="button" onClick={()=> onDelete(el.id)}>Delete</button>
+                <button type="button" onClick={()=> onDelete(el.id)} className={css.delete_button}><RiDeleteBin5Line size={24}/></button>
                </li>))}
                  </ul>   
         </div>
