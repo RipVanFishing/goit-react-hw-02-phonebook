950aaa2a9d71faaacab82a15c590002650008ded
Denis Serkhel <ripvanfish27@gmail.com>
2022-05-17 21:26:12 +0200

    немного стилей

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,10 +2,11 @@ import React from "react";
 import css from './SearchForm.module.css';
 
 const SearchForm = ({ value, onChange }) => (
-    
-<label>Find contacts by name
- <input value={value} onChange={onChange} name='find-name' type='text' />
- </label>
+<div className={css.form_box}>
+<label className={css.form_label}>Find contacts by name
+ <input value={value} onChange={onChange} name='find-name' type='text' className={css.filter_form} />
+        </label>
+        </div>
 )
    
     
